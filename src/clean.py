0046c57fc787a708534d7227fe95eeffe8a88250
Brandon <b1luu@ucsd.edu>
2026-02-13 16:08:31 -0800

    Remove Chinese characters from Category and Item

--- src/clean.py
+++ src/clean.py
@@ -7,26 +7,44 @@ raw = pd.read_csv("data/raw/raw.csv")
 
 #Manually drop columns
 
-cols_to_drop = {
+cols_to_drop = [
     "Time",
-    "Time Zone"
-    "Price Point Name"
-    "SKU"
-    "Transaction ID"
-    "Payment ID"
-    "Device Name"
-    "Notes"
-    "Gross Sales"
-    "Discounts"
-    "Net Sales"
-    "Employee"
-    "Fulfillment Note"
-    "Channel"
-    "Token"
-    "Card Brand"
-    "PAN Suffix"
-    "Dining Option"
-    "Location"
-    "Event Type"
-    "GTIN"
-}
+    "Time Zone",
+    "Price Point Name",
+    "SKU",
+    "Transaction ID",
+    "Payment ID",
+    "Device Name",
+    "Notes",
+    "Gross Sales",
+    "Discounts",
+    "Net Sales",
+    "Employee",
+    "Fulfillment Note",
+    "Channel",
+    "Token",
+    "Card Brand",
+    "Customer Name",
+    "Customer Reference ID",
+    "Customer ID",
+    "PAN Suffix",
+    "Dining Option",
+    "Location",
+    "Event Type",
+    "GTIN",
+    "Tax",
+    "Details",
+    "Count",
+    "Unit",
+    "Itemization Type",
+    "Commission",
+]
+
+clean = raw.drop(columns=cols_to_drop, errors="ignore")
+
+#Remove Chinese characters from Category and Item 
+
+
+
+clean.to_csv("data/trim/clean.csv")
+
