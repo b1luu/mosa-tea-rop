0ddeebe9d2ed59025f3791b896af8123158a0d5b
Brandon <b1luu@ucsd.edu>
2026-02-18 22:59:48 -0800

    initializing canonicalize.py

--- src/canonicalize.py
+++ src/canonicalize.py
@@ -1,1 +1,6 @@
 import pandas as pd 
+import re 
+
+clean = pd.read_csv("data/trim/clean.csv", low_memory=False)
+token_map = pd.read_csv("data/reference/modifier_token_map.csv")
+item_rules = pd.read_csv("data/reference/item_rules.csv")
